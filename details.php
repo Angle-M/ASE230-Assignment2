3a9a208406174decc02ed8d6298f23754ea4b60f
Angel Munoz <aim1310@icloud.com>
2022-09-07 16:58:49 -0400

    Base pages
    Basic pages for the assignment

---
+++ details.php
@@ -0,0 +1,208 @@
+<html lang="en">
+<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous">
+</head>
+
+<body>
+  <link rel="stylesheet" href="assets/css/detail.css">
+  <title>
+    <?php
+      echo "ASE 230 - Angel Munoz";
+    ?>
+  </title>
+  <div class="container text-center mb-5">
+    <h1>
+      <?php
+        echo "This is ASE 230 - Angel Munoz";
+      ?>
+    </h1>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6">
+        <div class="mb-2">
+          <?php
+            echo ' <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar7.png"alt="">';
+          ?>
+        </div>
+        <div class="mb-2 d-flex">
+          <h4 class="font-weight-normal">
+            <?php
+              echo "Angel Munoz";
+            ?>
+          </h4>
+          <div class="social d-flex ml-auto">
+            <p class="pr-2 font-weight-normal">
+              <?php
+                echo "Follow on:";
+              ?>
+            </p>
+            <a href="#" class="text-muted mr-1">
+              <?php
+                echo '<i class="fab fa-facebook-f"></i>';
+              ?>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <?php
+                echo '<i class="fab fa-twitter"></i>';
+              ?>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <?php
+                echo '<i class="fab fa-instagram"></i>';
+              ?>
+            </a>
+            <a href="#" class="text-muted">
+              <?php
+                echo '<i class="fab fa-linkedin"></i>'
+              ?>
+            </a>
+          </div>
+        </div>
+        <div class="mb-2">
+          <ul class="list-unstyled">
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Dream profession: ";
+                ?>
+              </span>
+              <label class="media-body">
+                <?php
+                  echo "Software Devolper or Full-Stack Deveolper";
+                ?>
+              </label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Dream company:";
+                ?>
+              </span>
+              <label class="media-body">
+                <?php
+                  echo "Anywhere that allows me to travel and work remotely";
+                ?>
+              </label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Email:";
+                ?>
+              </span>
+              <label class="media-body">
+                <?php
+                  echo "aim1310@icloud.com";
+                ?>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal">
+          <?php
+            echo "Short intro";
+          ?>
+        </h5>
+        <p>
+          <?php
+            echo "Along with your plans, you should consider developing an action orientation that will keep you motivated to move forward at all times. 
+            This requires a little self-discipline, but is a crucial component to achievement of any kind. Before starting any new activity, ask yourself
+             if that activity will move you closer to your goals. If the answer is no, you may want to reconsider doing it at that time.";
+          ?>
+        </p>
+        <div class="my-2 bg-light p-2">
+          <p class="font-italic mb-0">
+            <?php
+              echo "The price is something not necessarily defined as financial. It could be time, effort, sacrifice,
+                money or perhaps, something else.";
+            ?>
+          </p>
+        </div>
+        <div class="mb-2 mt-2 pt-1">
+          <h5 class="font-weight-normal">
+            <?php
+              echo "Top skills";
+            ?>
+          </h5>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:81%" aria-valuenow="81" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                <?php
+                  echo "Communication";
+                ?>
+              </div>
+              <span class="progress-bar-number">
+                <?php
+                  echo "81%";
+                ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                <?php
+                  echo "Information Technologies";
+                ?>
+              </div>
+              <span class="progress-bar-number">
+                <?php
+                  echo "90%";
+                ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:72%" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                <?php
+                  echo "Education";
+                ?>
+              </div>
+              <span class="progress-bar-number">
+                <?php
+                  echo "72%";
+                ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <h5 class="font-weight-normal">
+          <?php
+            echo "Fun fact";
+          ?>
+        </h5>
+        <p>
+          <?php
+            echo "I also watch Super Sentai (Japanese Power Rangers) and I'm going to Paramore this fall";
+          ?>
+        </p>
+      </div>
+      <a href="index.php">
+        <?php
+          echo '<button>
+          Main Page
+          </button>';
+        ?>
+      </a>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
